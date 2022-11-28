c9c0e21e72618764a92e71de5893341ee3e363e5
Narmis-E <109248529+Narmis-E@users.noreply.github.com>
2022-11-28 16:44:49 +1030

    updates packages.sh

--- packages.sh
+++ packages.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+my_dir=`dirname $0`
+
 yay -S catppuccin-gtk-theme-mocha zscroll-git
 sudo pacman -S lxappearance-gtk3 lxsession brightnessctl papirus-icon-theme playerctl
 
@@ -7,3 +9,4 @@ mkdir ~/Downloads/git && cd ~/Downloads/git/
 git clone https://github.com/catppuccin/papirus-folders.git && cd papirus-folders
 sudo cp -r src/* /usr/share/icons/Papirus
 ./papirus-folders -C cat-mocha-red --theme Papirus
+sudo mv $my_dir/fonts/'Iosevka Term Nerd Font Complete.ttf' /usr/share/fonts
