d316a4f2950125a3147db28ec81c015bf3b9fac2
Narmis-E <109248529+Narmis-E@users.noreply.github.com>
2022-11-28 16:47:36 +1030

    updated

--- packages.sh
+++ packages.sh
@@ -3,7 +3,7 @@
 my_dir=`dirname $0`
 
 yay -S catppuccin-gtk-theme-mocha zscroll-git
-sudo pacman -S lxappearance-gtk3 lxsession brightnessctl papirus-icon-theme playerctl
+sudo pacman -S bspwm sxhkd polybarlxappearance-gtk3 lxsession brightnessctl papirus-icon-theme playerctl
 
 mkdir ~/Downloads/git && cd ~/Downloads/git/
 git clone https://github.com/catppuccin/papirus-folders.git && cd papirus-folders
