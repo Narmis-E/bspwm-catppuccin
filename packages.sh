bc4ecb956997594fff4da59b6cc7371c55c98d12
narmis-e <narmisecurb@gmail.com>
2022-12-02 15:44:13 +1030

    added package requirements

--- packages.sh
+++ packages.sh
@@ -3,7 +3,7 @@
 my_dir=`dirname $0`
 
 yay -S catppuccin-gtk-theme-mocha zscroll-git
-sudo pacman -S bspwm sxhkd polybarlxappearance-gtk3 lxsession brightnessctl papirus-icon-theme playerctl
+sudo pacman -S bspwm sxhkd polybarlxappearance-gtk3 lxsession brightnessctl papirus-icon-theme playerctl rofi nitrogen tint2
 
 mkdir ~/Downloads/git && cd ~/Downloads/git/
 git clone https://github.com/catppuccin/papirus-folders.git && cd papirus-folders
