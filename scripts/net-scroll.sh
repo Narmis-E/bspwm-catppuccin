4da0838b05a18a7f5ff890df582f14dc5c15dd6d
narmis-e <narmisecurb@gmail.com>
2022-12-02 16:09:20 +1030

    removed unused scripts

--- scripts/net-scroll.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#ssid=`nmcli -t -f name connection show --active`
-ipaddr=`ip a | grep "scope global" | grep -Po '(?<=inet )[\d.]+'`
-#net="$ssid $ipaddr" 
-echo "$ipaddr" | zscroll -l 5
