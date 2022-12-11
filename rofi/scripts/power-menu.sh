53511e74e5e42aa4cef32daad2c118126b50bf7d
narmis-e <narmisecurb@gmail.com>
2022-12-12 02:37:53 +1030

    replaced xfce login/logout/reboot commands

--- rofi/scripts/power-menu.sh
+++ rofi/scripts/power-menu.sh
@@ -3,10 +3,10 @@
 chosen=$(printf "  Power Off\n  Restart\n  Lock\n  Sign Out" | rofi -dmenu -i -theme-str '@import "simple-tokyonight.rasi"')
 
 case "$chosen" in
-	"  Power Off") xfce4-session-logout --halt --fast ;;
-	"  Restart") xfce4-session-logout --reboot --fast ;;
+	"  Power Off") systemctl poweroff ;;
+	"  Restart") systemctl reboot ;;
 	"  Lock") i3lock-fancy ;;
-	"  Sign Out") xfce4-session-logout --logout ;;
+	"  Sign Out") bspc quit ;;
 
 	*) exit 1 ;;
 esac
